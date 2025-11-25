5984ec18b84c1cc4e1e11032e5d4c0eefced8006
Gleb Mosko <gleb.yandex.ru>
2025-11-25 18:46:14 +0300

    Initial Commit

---
+++ Exam37Lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam37Lesson
+//
+//  Created by Gleb on 11/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
